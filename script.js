c51737b84d2d9f43c143df48a67c4427f60719aa
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-03-31 15:41:59 -0400

    new quotes can be seen on refresh and api is working

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const RandQuote = `http://api.quotable.io/random`;
+const randQuote = `http://api.quotable.io/random`;
+const showQuote = document.getElementById(`quoteDisplay`);
+// const quoteInput
 
 function getQuote() {
-  fetch(RandQuote)
+  return fetch(randQuote)
     .then(response => response.json())
     .then(data => data.content);
 }
+
+async function showNewQuote() {
+  const quote = await getQuote();
+  showQuote.innerText = quote;
+}
+
+showNewQuote();
